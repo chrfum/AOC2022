a2138046dbd2c5ac06379a4509d3064eac8c040f
Christian Fumagalli <73828171+chrfum@users.noreply.github.com>
2022-12-07 13:47:18 +0100

    day02

---
+++ java/src/Day02.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day02 {
+
+    public static void main(String []args) {
+        BufferedReader reader;
+        int score = 0;
+
+        try {
+            reader = new BufferedReader(new FileReader("input.txt"));
+            String line = reader.readLine();
+
+            while (line != null) {
+                char resultExpected = line.charAt(2);
+                char elfShape = line.charAt(0);
+
+                if (resultExpected == 'X') {
+                    score += 0;
+                    if (elfShape == 'A') score += 3;
+                    else if (elfShape == 'B') score += 1;
+                    else score += 2;
+                } else if (resultExpected == 'Y') {
+                    score += 3;
+                    if (elfShape == 'A') score += 1;
+                    else if (elfShape == 'B') score += 2;
+                    else score += 3;
+                } else {
+                    score += 6;
+                    if (elfShape == 'A') score += 2;
+                    else if (elfShape == 'B') score += 3;
+                    else score += 1;
+                }
+
+                line = reader.readLine();
+            }
+
+            reader.close();
+            System.out.println("The total score is " + score);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
